b0ee398418fdb4d74c1a0edd5d0f50bf0f1ba9f2
Alejandro <aletomasperren@gmail.com>
2024-11-13 16:26:14 -0300

    Start Whit server and dependencies

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction, Request, Response }  from "express";
+import createHttpError, { isHttpError } from "http-errors";
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    next(createHttpError(404, "Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occurred";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
+
+
+export default app;
+
---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import app from "./app";
+import mongoose from "mongoose";
+import env from "./utils/validateEnv"
+
+const PORT = env.PORT
+
+mongoose.connect(env.MONGO_CONNECTION)
+    .then(()=> {
+        console.log("Conectado Al servidor de Mongoose");
+        app.listen(PORT, ()=> {
+            console.log("Servidor Ejecutandose en el Puerto", PORT)
+        })
+
+
+    })
+    .catch(console.error);
